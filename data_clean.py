a6b60b783721bf54572733d934fb9038efe6b129
Ankit Tare <atare@andrew.cmu.edu>
2016-04-19 19:36:15 -0400

    XML Handler and Data Cleaning

---
+++ data_clean.py
@@ -0,0 +1,41 @@
+import re;
+def clean():
+    #TODO: fix bug of  &quot;infinity&quot; line truncating
+    f=open("C:\\Users\\Ankit\\Documents\\Programming\\devpy\\SentimenAnalysis_NLP_DataMining\\D1.txt", 'r', encoding="utf8");
+    o=open("C:\\Users\\Ankit\\Documents\\Programming\\devpy\\SentimenAnalysis_NLP_DataMining\\cleaned_d1.txt", 'w', encoding="utf8");
+    flag=False;
+    for lines in f:
+        if lines!="":
+            if(flag!=True):
+                s=lines;
+            if("<label>" in lines):
+                s=s.strip()+'</label>';
+            elif("<rating>" in lines):
+                s=s.strip()+"</rating>"
+            elif("<TEXT>" in lines):
+                flag=True;
+                o.write(s.strip().replace("<br><br>", "") + "\n");
+                s="";
+                continue;
+            elif(flag == True and "</DOC>" not in lines):
+                s += lines.strip() + " ";
+                continue;
+            elif("</DOC>" in lines):
+
+                s=re.sub('[^0-9a-zA-Z ]+', "", s)
+                s =s.lower()+ "</TEXT>\n</DOC>"
+                flag=False
+            if(flag!=True):
+                o.write(s.strip().replace("<br><br>","")+"\n");
+            else:
+                o.write(s.strip().replace("<br><br>", "") + " ");
+            o.flush()
+    o.close()
+
+def clean_text(s):
+    news=""
+    for i in range(0, len(s)):
+        if ord(s[i].lower()) >= 97 or ord(s[i].lower()) <= 123 or s[i]==" ":
+            news+=s[i]
+    return news
+clean()
---
+++ xml_handler.py
@@ -0,0 +1,73 @@
+import xml.sax
+
+from SentimenAnalysis_NLP_DataMining import model
+
+
+class xml_handler( xml.sax.ContentHandler ):
+
+    def __init__(self):
+        self.CurrentData = ""
+        self.docid = ""
+        self.product_class = ""
+        self.rating = ""
+        self.label = ""
+        self.text = ""
+        self.product_class = {};
+    # Call when an element starts
+
+    def startElement(self, tag, attributes):
+        self.CurrentData = tag
+        if tag == "DOC":
+            print("*****DOC*****")
+
+   # Call when an elements ends
+    def endElement(self, tag):
+        pc="";
+        rating="";
+        t="";
+        l="";
+        d="";
+        if self.CurrentData == "CLASS":
+            pc=self.product_class;
+        elif self.CurrentData == "rating":
+            rating=self.rating
+        elif self.CurrentData == "TEXT":
+            t=self.text;
+        elif self.CurrentData == "label":
+            l=self.label;
+        elif self.CurrentData == "DOCID":
+            d=self.docid;
+        m=model(d,l,rating,pc,t)
+        if(l not in self.product_class.keys()):
+            self.product_class[l]=[];
+        self.product_class[l].append(m);
+        self.CurrentData="";
+
+   # Call when a character is read
+    def characters(self, content):
+        if self.CurrentData == "DOCID":
+            self.docid = content
+        elif self.CurrentData == "CLASS":
+            self.product_class = content
+        elif self.CurrentData == "rating":
+            self.rating = content
+        elif self.CurrentData == "label":
+            self.label = content
+        elif self.CurrentData == "TEXT":
+            self.text = content
+
+    def __iter__(self):
+        return self.product_class;
+
+def main():
+    # create an XMLReader
+    parser = xml.sax.make_parser()
+    # turn off namepsaces
+    parser.setFeature(xml.sax.handler.feature_namespaces, 0)
+    # override the default ContextHandler
+    Handler = xml_handler()
+    parser.setContentHandler( Handler );
+    parser.parse("C:\\Users\\Ankit\\Documents\\Programming\\devpy\\SentimenAnalysis_NLP_DataMining\\cleaned_d1.txt");
+    print(Handler.__iter__());
+
+main();
